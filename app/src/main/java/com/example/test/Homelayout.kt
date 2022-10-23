5dc967561febbee923a421d2ad75af26b3082848
Nurzhan Mussabekov <200107138@stu.sdu.edu.kz>
2022-10-24 01:33:24 +0600

    added button action to reaction_layout fragment in Homelayout.kt

---
+++ app/src/main/java/com/example/test/Homelayout.kt
@@ -0,0 +1,44 @@
+package com.example.test
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [home_layout.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class Homelayout : Fragment() {
+    // TODO: Rename and change types of parameters
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.home_layout, container, false)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        var recyclerView: Button = view.findViewById(R.id.startbutton)
+        recyclerView.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_home_layout_to_reaction_layout)
+        }
+    }
+
+}
