f3af888272fa6c8cb0af207291775c2fe202e314
Nurzhan Mussabekov <200107138@stu.sdu.edu.kz>
2022-10-24 20:41:32 +0600

    added navdrawer_menu.xml

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -2,27 +2,36 @@ package com.example.test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.test.databinding.ActivityMainBinding
+import com.example.test.databinding.HomeLayoutBinding
 
 class MainActivity : AppCompatActivity() {
-
-
+    lateinit var binding : ActivityMainBinding
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        drawerLayout = binding.drawerLayout
 
         val navController = this.findNavController(R.id.fragment)
-        NavigationUI.setupActionBarWithNavController(this,navController)
+
+        NavigationUI.setupActionBarWithNavController(this,navController, drawerLayout)
+
+        NavigationUI.setupWithNavController(binding.navView, navController)
 
     }
+
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.fragment)
-        return navController.navigateUp()
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 
 }
