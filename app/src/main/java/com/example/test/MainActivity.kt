a6b59a018e64aeaa0940b080dc7f1055cb7e23cf
Nurzhan Mussabekov <200107138@stu.sdu.edu.kz>
2022-10-24 18:09:45 +0600

    added UP button

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.test
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -14,8 +16,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val navController = this.findNavController(R.id.fragment)
+        NavigationUI.setupActionBarWithNavController(this,navController)
 
-
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.fragment)
+        return navController.navigateUp()
     }
 
 }
