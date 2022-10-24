78f3b6c240204f18b88a93c1e389a2945de439e2
Nurzhan Mussabekov <200107138@stu.sdu.edu.kz>
2022-10-24 19:54:21 +0600

    added options menu

--- app/src/main/java/com/example/test/Homelayout.kt
+++ app/src/main/java/com/example/test/Homelayout.kt
@@ -1,13 +1,15 @@
 package com.example.test
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import androidx.recyclerview.widget.RecyclerView
+import com.example.test.databinding.HomeLayoutBinding
+import com.example.test.databinding.ReactionLayoutBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -19,22 +21,24 @@ private const val ARG_PARAM2 = "param2"
  * Use the [home_layout.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Homelayout : Fragment() {
+class Homelayout : Fragment(R.layout.home_layout) {
     // TODO: Rename and change types of parameters
 
+    private lateinit var binding: HomeLayoutBinding
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu,menu)
+    }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.home_layout, container, false)
-
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, requireView().findNavController())
+                || super.onOptionsItemSelected(item)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setHasOptionsMenu(true)
         var recyclerView: Button = view.findViewById(R.id.startbutton)
         recyclerView.setOnClickListener {
             Navigation.findNavController(view).navigate(R.id.action_home_layout_to_reaction_layout)
