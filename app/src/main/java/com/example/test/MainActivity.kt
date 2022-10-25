124f70966df9d8304141bfa36e97fcdb24489ca6
Nurzhan Mussabekov <200107138@stu.sdu.edu.kz>
2022-10-25 11:45:40 +0600

    design changes in home_layout.xml and added switch

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.widget.SwitchCompat
+import androidx.core.content.ContextCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -14,6 +17,9 @@ import com.example.test.databinding.HomeLayoutBinding
 class MainActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainBinding
     private lateinit var drawerLayout: DrawerLayout
+    private lateinit var switchOnOff: SwitchCompat
+    private lateinit var tvSwitchYes: TextView
+    private lateinit var tvSwitchNo: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +33,21 @@ class MainActivity : AppCompatActivity() {
 
         NavigationUI.setupWithNavController(binding.navView, navController)
 
+        switchOnOff = findViewById<SwitchCompat>(R.id.switchOnOff)
+        tvSwitchYes = findViewById<TextView>(R.id.tvSwitchYes)
+        tvSwitchNo = findViewById<TextView>(R.id.tvSwitchNo)
+        switchOnOff.setOnCheckedChangeListener { _, checked ->
+            when {
+                checked -> {
+                    tvSwitchYes.setTextColor(ContextCompat.getColor(this,R.color.blue))
+                    tvSwitchNo.setTextColor(ContextCompat.getColor(this,R.color.white))
+                }
+                else -> {
+                    tvSwitchYes.setTextColor(ContextCompat.getColor(this,R.color.white))
+                    tvSwitchNo.setTextColor(ContextCompat.getColor(this,R.color.blue))
+                }
+            }
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
