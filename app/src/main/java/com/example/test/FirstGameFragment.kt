113fa24600e8192896fc15f165f37eedfb952b99
Nurzhan Mussabekov <200107138@stu.sdu.edu.kz>
2022-10-29 00:59:21 +0600

    unfull logic of firstgame

--- app/src/main/java/com/example/test/FirstGameFragment.kt
+++ app/src/main/java/com/example/test/FirstGameFragment.kt
@@ -6,12 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
 import com.example.test.databinding.FragmentFirstGameBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import java.lang.System.exit
 
 
 class FirstGameFragment : Fragment() {
-
+    private val viewModel: FirstGameViewModel by viewModels()
     private lateinit var binding: FragmentFirstGameBinding
 
     override fun onCreateView(
@@ -24,15 +27,49 @@ class FirstGameFragment : Fragment() {
 
         return binding.root
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         // Setup a click listener for the Submit and Skip buttons.
-        binding.firstgamestarttext.setOnClickListener { }
-        // Update the UI
+        binding.firstgamestarttext.setOnClickListener {onSubmitWord()
+            // Update the UI
+            if (binding.firstgamestarttext.getTag().equals("white")) {
+                binding.firstgamestarttext.setText("")
+                binding.firstgamestarttext.setBackgroundResource(R.drawable.first_game_start_green)
+            }
+
+        }
+    }
+    private fun onSubmitWord() {
+
+
+
+            if (viewModel.nextWord()) {
+                updateNextWordOnScreen()
+            } else {
+                showFinalScoreDialog()
+            }
 
     }
 
+    private fun showFinalScoreDialog() {
+        MaterialAlertDialogBuilder(requireContext())
+            .setTitle(getString(R.string.congratulations))
+            .setMessage(getString(R.string.you_scored, viewModel.score))
+            .setCancelable(false)
+            .setNegativeButton(getString(R.string.exit)) { _, _ ->
+                exitGame()
+            }
+            .setPositiveButton(getString(R.string.play_again)) { _, _ ->
+                restartGame()
+            }
+            .show()
+    }
 
-
+    private fun updateNextWordOnScreen() {
+        binding.firstgamestarttext.setTag(viewModel.currentTag)
+    }
 }
+
+
--- app/src/main/java/com/example/test/FirstGameViewModel.kt
+++ app/src/main/java/com/example/test/FirstGameViewModel.kt
@@ -1,7 +1,33 @@
 package com.example.test
 
 import androidx.lifecycle.ViewModel
+import com.example.android.unscramble.ui.game.MAX_NO_OF_WORDS
 
 class FirstGameViewModel : ViewModel(){
+    private var _score = 0
+    val score: Int
+        get() = _score
 
+    private var _currentFirstGameCount = 0
+    val currentFirstGameCount: Int
+        get() = _currentFirstGameCount
+
+    private lateinit var _currentTag: String
+    val currentTag: String
+        get() = _currentTag
+
+    private fun getNextWord() {
+
+            ++_currentWordCount
+            wordsList.add(currentWord)
+
+    }
+
+
+    fun nextWord(): Boolean {
+        return if (_currentFirstGameCount < MAX_NO_OF_WORDS) {
+            getNextWord()
+            true
+        } else false
+    }
 }
