8e63477de19feba0cf6f8f38a69cfc27e2722ec3
Nurzhan Mussabekov <200107138@stu.sdu.edu.kz>
2022-11-06 15:29:37 +0600

    font changes

--- app/src/main/java/com/example/test/TrainingFragment.kt
+++ app/src/main/java/com/example/test/TrainingFragment.kt
@@ -7,16 +7,6 @@ import androidx.navigation.findNavController
 
 import com.example.test.databinding.FragmentTrainingBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [home_layout.newInstance] factory method to
- * create an instance of this fragment.
- */
 class TrainingFragment : Fragment(R.layout.fragment_training) {
     // TODO: Rename and change types of parameters
     private lateinit var binding: FragmentTrainingBinding
